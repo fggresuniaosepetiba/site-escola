71602a1d8a92752840df88b62534ad108c48a556
FellypeGC <gabrielfellypesteve@gmail.com>
2026-05-13 23:56:58 -0300

    add vars deploy

---
+++ js/gallery/supabase-env.js
@@ -0,0 +1,4 @@
+window.APP_ENV = {
+  SUPABASE_URL: "https://qiisceshgtbgjdiauqsu.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpaXNjZXNoZ3RiZ2pkaWF1cXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTQxMzMsImV4cCI6MjA5NDI3MDEzM30.6t-gjPRoYXoo-_1fGB2GnWiYBMGf2e5jA7VYA9xloow",
+};
